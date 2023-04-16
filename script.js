a43139ba4dbed5eca426a3274439eb66ff21bac1
brunoprosa <bruno.prosa01@gmail.com>
2023-04-16 19:05:03 -0300

    parte obrigatória

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 axios.defaults.headers.common['Authorization'] = 'JUN0yDqM4gl6X2WCNJqGitQy';
 
 let nome = '';
+let nomo = {};
 
 pegarNome();
 
 function pegarNome(){
     nome = prompt("Qual o seu nome?");
-    nome = {name: nome};
+    nomo = {name: nome};
     console.log(nome);
-    const promise = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants', nome);
-    promise.then(renderizarChat);
+    const promise = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants', nomo);
+    promise.then(iniciar);
     promise.catch(pegarNome);
+}
+
+function iniciar(){
     setInterval(conexao,5000);
+    setInterval(carregarMensagem, 3000);
+    carregarMensagem();
 }
 
 function conexao(){
-    axios.post('https://mock-api.driven.com.br/api/vm/uol/status', nome);
+    axios.post('https://mock-api.driven.com.br/api/vm/uol/status', nomo);
+    console.log('oi');
 }
 
 function enviarMensagem(){
@@ -29,10 +36,24 @@ function enviarMensagem(){
     console.log(mensagem);
     const promise = axios.post('https://mock-api.driven.com.br/api/vm/uol/messages',mensagem);
     //mensagem.reset();
+    promise.then(carregarMensagem);
+}
+
+function carregarMensagem(){
+    const promise = axios.get('https://mock-api.driven.com.br/api/vm/uol/messages');
     promise.then(renderizarChat);
 }
 
 function renderizarChat(resposta){
-    console.log(resposta.data);
-
+    console.log(resposta);
+    resposta = resposta.data;
+    let mensagens = document.querySelector('.mensagens');
+    for (let i = 0; i < resposta.length; i++) {
+        if (resposta[i].type === "status"){
+            mensagens.innerHTML += `<div class="mensagem cinza" data-test="message"><strong class="horario">${resposta[i].time}</strong> <strong class="nome">${resposta[i].from}</strong> ${resposta[i].text}</div>`;
+        }
+        if (resposta[i].type === "message"){
+            mensagens.innerHTML += `<div class="mensagem" data-test="message"><strong class="horario">${resposta[i].time}</strong> <strong class="nome">${resposta[i].from}</strong> para <strong class="nome">${resposta[i].to}</strong>: ${resposta[i].text}</div>`;
+        }
+    }
 }
